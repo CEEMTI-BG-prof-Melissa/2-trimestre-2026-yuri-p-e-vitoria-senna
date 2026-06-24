1385b4c231986ca2e50db7b2842897f06af77e41
YuriDnt <yuri.posouza@aluno.edu.es.gov.br>
2026-06-24 11:45:39 -0300

    index.php

--- Aula02 - php/index.php
+++ Aula02 - php/index.php
@@ -2,13 +2,15 @@
 <html lang="pt-br">
     <head>
         <meta charset "UTF-8">
-        <title> Aula 02 PHP :P</title>
+        <title> Aula 02 PHP :D</title>
     </head>
     <body>
         <?php
-            $nome="vitoria";
-            $idade="17";
-            echo "Meu nome é " .$nome ." e tenho " .$idade ." anos";
+            $nome1="Yuri";
+            $idade1="16";
+            $nome2="Vitória";
+            $idade2="17";
+            echo "Meu nome é " .$nome1 ." e tenho " .$idade1 ." anos, e estou com minha companheira ".$nome2. " que tem ".$idade2 ." anos";
         ?>
     </body>
 </html>        
