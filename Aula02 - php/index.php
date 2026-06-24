ab314c153baad2f5622842c9fb48acde61c584d6
YuriDnt <yuri.posouza@aluno.edu.es.gov.br>
2026-06-24 11:18:03 -0300

    index.php

---
+++ Aula02 - php/index.php
@@ -0,0 +1,14 @@
+<!Doctype HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset "UTF-8">
+        <title> Aula 02 </title>
+    </head>
+    <body>
+        <?php
+            $nome="Yuri";
+            $idade="16";
+            echo "Meu nome é " .$nome ." e tenho " .$idade ." anos";
+        ?>
+    </body>
+</html>        
